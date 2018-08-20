a82c3d09e6f4b723688606622d3dcdcc17e45456
Håvard Berland <havb@equionor.com>
2018-08-20 16:25:08 +0200

    API change, get_ens_smry() -> get_smry().
    Rewritten to just wrap around eclsum.pandas_frame

--- src/fmu/ensemble/ensemble.py
+++ src/fmu/ensemble/ensemble.py
@@ -236,60 +236,36 @@ class ScratchEnsemble(object):
             dflist.append(dframe)
         return pd.concat(dflist)
 
-    def get_ens_smry(self, vector_match=None):
+    def get_smry(self, time_index=None, column_keys=None, stacked=True):
         """
-        Returns a datframe of all eclipse summary
-        vectors for all realizations in the ensemble.
+        Aggregates summary data from all realizations.
+
+        Wraps around Realization.get_smry() which wraps around
+        ert.ecl.EclSum.pandas_frame()
 
         Args:
-            vector_match: `Optional`. String (or list of strings)
-                with wildcard filter. If None, all vectors are returned
+            time_index: list of DateTime if interpolation is wanted
+               default is None, which returns the raw Eclipse report times
+            column_keys: list of column key wildcards
+            stacked: boolean determining the dataframe layout. If
+                true, the realization index is a column, and dates are repeated
+                for each realization in the DATES column.
+                If false, a dictionary of dataframes is returned, indexed
+                by vector name, and with realization index as columns.
+                This only works when time_index is the same for all
+                realizations. Not implemented yet!
 
         Returns:
-            A DataFame of summary vectors for the ensemble.
-
-        Todo:
-            *  Throw an warning if the requested summary keyword does
-                not exists.
-            *  freq (str): `Optional`. Date freqency of dataframe. 'D' - daily,
-                'W' - weekly, 'M' - en of Month, 'MS' - start of month,
-                'A' - end of year, 'AS' - end of year. Default 'MS'
-            *  agg (str): `Optional`. Data aggredation. 'mean' or 'sum'.
-                Default 'mean'.
-            *  Filter based on time range interval
+            A DataFame of summary vectors for the ensemble, or
+            a dict of dataframes if stacked=False
         """
-
-        # get matches based on input string or list of strings
-        flat_vector_match = self.get_smrykeys(vector_match=vector_match)
-
-        # check if the vector have been cached
-        if not self._ens_df.empty:
-            smrykeys = [key for key in flat_vector_match if key not in
-                        self._ens_df.columns.values.tolist()]
-        else:
-            smrykeys = flat_vector_match
-
-        # read the summary keys for each realization if the list is not empty
-        if smrykeys:
-            ens_df = pd.DataFrame()
-            for index, realization in self._realizations.items():
-                dframe = realization.get_smryvalues(props_wildcard=smrykeys)
-                dframe = dframe.resample('D').mean().fillna(method='ffill')
-                if self._ens_df.empty:
-                    # add realisation number and ensemble name
-                    dframe.insert(0, 'REAL', index)
-                    dframe.insert(1, 'ENS', self._name)
-                ens_df = pd.concat([ens_df, dframe])
-        else:
-            return self._ens_df[flat_vector_match].copy()
-
-        # cache the dataframe
-        if self._ens_df.empty:
-            self._ens_df = ens_df
-        else:
-            self._ens_df = pd.concat([self._ens_df, ens_df], axis=1)
-
-        return self._ens_df[flat_vector_match].copy()
+        dflist = []
+        for index, realization in self._realizations.items():
+            dframe = realization.get_smry(time_index=time_index,
+                                          column_keys=column_keys)
+            dframe['REAL'] = index
+            dflist.append(dframe)
+        return pd.concat(dflist)
 
     def get_wellnames(self, well_match=None):
         """
--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -104,10 +104,12 @@ def test_ensemble_ecl():
     assert len(reekensemble.get_smrykeys('BOGUS')) == 0
 
     # reading ensemble dataframe
-    assert len(reekensemble.get_ens_smry(['FOPR']).columns) == 1
-    assert len(reekensemble.get_ens_smry('FOP*').columns) == 9
-    assert len(reekensemble.get_ens_smry(['FGPR', 'FOP*']).columns) == 10
-    assert len(reekensemble.get_ens_smry(['FGPR', 'FOP*']).index) == 3294
+
+    # When asking the ensemble for FOPR, we also get REAL as a column in return:
+    assert len(reekensemble.get_smry(column_keys=['FOPR']).columns) == 2
+    assert len(reekensemble.get_smry(column_keys=['FOP*']).columns) == 10
+    assert len(reekensemble.get_smry(column_keys=['FGPR', 'FOP*']).columns) == 11
+    assert len(reekensemble.get_smry(column_keys=['FGPR', 'FOP*']).index) == 1700
 
     # eclipse well names list
     assert len(reekensemble.get_wellnames('OP*')) == 5
