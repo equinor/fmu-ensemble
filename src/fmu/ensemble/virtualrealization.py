8179e43ac573880d5ce709af97113248dcf05d01
Håvard Berland <havb@equinor.com>
2018-08-28 23:02:22 +0200

    forgot to add important file in last commit

---
+++ src/fmu/ensemble/virtualrealization.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import re
+import glob
+import json
+import numpy
+import pandas as pd
+
+import ert.ecl
+from fmu import config
+
+fmux = config.etc.Interaction()
+logger = fmux.basiclogger(__name__)
+
+if not fmux.testsetup():
+    raise SystemExit()
+
+class VirtualRealization(object):
+    """A computed or archived realization.
+
+    Computed or archived, one cannot assume to have access to the file
+    system containing original data.
+
+    Datatables that in a ScratchRealization was available through the
+    files dataframe, is now available as dataframes in a dict accessed
+    by the localpath in the files dataframe from ScratchRealization-
+
+    """
+    def __init__(self, description=None, data={}):
+        self._description = description
+        self.data = data
+    
+    def keys(self):
+        return self.data.keys()
+
+    def __getitem__(self, localpath):
+        return self.get_df(localpath)
+
+    def append(self, key, dataframe, overwrite=False):
+        if key in self.data.keys() and not overwrite:
+            logger.warning('Ignoring ' + key + ', data already exists')
+            return
+        self.data[key] = dataframe
+
+    def __repr__(self):
+        """Represent the realization. Show only the last part of the path"""
+        return "<VirtualRealization, {}>".format(self._description)
+
+    def get_df(self, localpath):
+        """Access the internal datastore which contains dataframes or dicts
+
+        Shorthand is allowed, if the fully qualified localpath is
+            'share/results/volumes/simulator_volume_fipnum.csv'
+        then you can also get this dataframe returned with these alternatives:
+         * simulator_volume_fipnum
+         * simulator_volume_fipnum.csv
+         * share/results/volumes/simulator_volume_fipnum
+
+        but only as long as there is no ambiguity. In case of ambiguity, a
+        ValueError will be raised.
+
+        Args:
+            localpath: the idenfier of the data requested
+
+        Returns:
+            dataframe or dictionary
+        """
+        if localpath in self.data.keys():
+            return self.data[localpath]
+
+        # Allow shorthand, but check ambiguity
+        basenames = [os.path.basename(x) for x in self.data.keys()]
+        if basenames.count(localpath) == 1:
+            shortcut2path = {os.path.basename(x): x for x in self.data.keys()}
+            return self.data[shortcut2path[localpath]]
+        noexts = [''.join(x.split('.')[:-1]) for x in self.data.keys()]
+        if noexts.count(localpath) == 1:
+            shortcut2path = {''.join(x.split('.')[:-1]): x
+                             for x in self.data.keys()}
+            return self.data[shortcut2path[localpath]]
+        basenamenoexts = [''.join(os.path.basename(x).split('.')[:-1])
+                          for x in self.data.keys()]
+        if basenamenoexts.count(localpath) == 1:
+            shortcut2path = {''.join(os.path.basename(x).split('.')[:-1]): x
+                             for x in self.data.keys()}
+            return self.data[shortcut2path[localpath]]
+        raise ValueError(localpath)
+        
+
+    @property
+    def parameters(self):
+        return self.data['parameters.txt']
+
+    @property
+    def name(self):
+        return self._description
