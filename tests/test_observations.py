41fa5aee54af0522e17d9a1f36382fc3879435d4
Håvard Berland <havb@equinor.com>
2018-10-09 10:26:58 +0200

    move observation testing to its own function

---
+++ tests/test_observations.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""Testing fmu-ensemble."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import numpy
+import pandas as pd
+
+import pytest
+
+from fmu import config
+from fmu.ensemble import ScratchEnsemble, ScratchRealization
+
+fmux = config.etc.Interaction()
+logger = fmux.basiclogger(__name__)
+
+if not fmux.testsetup():
+    raise SystemExit()
+
+
+def test_observation_import():
+    if '__file__' in globals():
+        # Easen up copying test code into interactive sessions
+        testdir = os.path.dirname(os.path.abspath(__file__))
+    else:
+        testdir = os.path.abspath('.')
+
+    reekensemble = ScratchEnsemble('reektest',
+                                   testdir +
+                                   '/data/testensemble-reek001/' +
+                                   'realization-*/iter-0')
+
+    obs = reekensemble.load_observations(testdir +
+                                         '/data/testensemble-reek001/' +
+                                         '/share/observations/' +
+                                         'observations.yaml')
+
+    assert len(obs.keys()) == 2
+    df_mismatch = reekensemble.ensemble_mismatch()
+
+    assert len(df_mismatch.columns) == 7
