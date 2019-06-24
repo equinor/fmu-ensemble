bfe75fac65ac6335a87cf80aef3ae91ba77c07ac
Anders Fredrik Kiær <akia@statoil.com>
2019-06-24 21:56:56 +0200

    Map returns iterator on Python3

--- src/fmu/ensemble/ensembleset.py
+++ src/fmu/ensemble/ensembleset.py
@@ -478,7 +478,7 @@ class EnsembleSet(object):
 
         CODE DUPLICATION from realization.py
         """
-        basenames = map(os.path.basename, self.keys())
+        basenames = [os.path.basename(key) for key in self.keys()]
         if basenames.count(shortpath) == 1:
             short2path = {os.path.basename(x): x for x in self.keys()}
             return short2path[shortpath]
