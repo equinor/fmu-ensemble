79814736ce666d215b3413f5f3cfa5381237cbae
Håvard Berland <havb@equinor.com>
2020-03-16 12:45:22 +0100

    Resolve DeprecationWarning on MutableMapping

--- src/fmu/ensemble/realization.py
+++ src/fmu/ensemble/realization.py
@@ -18,7 +18,6 @@ import glob
 import json
 from datetime import datetime, date, time
 import calendar
-import collections
 import dateutil
 
 import yaml
@@ -1698,10 +1697,17 @@ def flatten(dictionary, parent_key="", sep="_"):
 
     Code from stackoverflow.
     """
+    # pylint: disable=import-outside-toplevel
+    try:
+        # Python3.3^
+        from collections.abc import MutableMapping
+    except ImportError:
+        # Python 2.7:
+        from collections import MutableMapping
     items = []
     for key, value in dictionary.items():
         new_key = parent_key + sep + key if parent_key else key
-        if isinstance(value, collections.MutableMapping):
+        if isinstance(value, MutableMapping):
             items.extend(flatten(value, new_key, sep=sep).items())
         else:
             items.append((new_key, value))
