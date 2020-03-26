60d5421574cc58198064630951b15dd40952bc6e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-26 22:28:42 +0000

    Deploy equinor/fmu-ensemble to github.com/equinor/fmu-ensemble.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.2.dev11+ga8d9241',
+    VERSION: '1.2.2.dev12+ga521509',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
