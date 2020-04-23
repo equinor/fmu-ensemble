0d42ccc7f689370a39b60dee5cd42388624f682a
Håvard Berland <havb@equinor.com>
2020-04-23 11:07:53 +0200

    Solve setup.py issue when LANG!=utf-8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except ImportError:
 with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.rst") as history_file:
+with open("HISTORY.rst", encoding="utf-8") as history_file:
     history = history_file.read()
 
 REQUIREMENTS = [
