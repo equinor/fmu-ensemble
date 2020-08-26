4d20e22ffe36442650fc87dbaf5e19d20afd009a
fmuensemble-github-action <fmuensemble-github-action>
2020-08-26 17:16:25 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.6.dev5+gaa84885',
+    VERSION: '1.2.6.dev6+g7dd07eb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
