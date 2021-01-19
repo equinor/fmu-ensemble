4a39f1984d4b862853300f8e75711f89d7f22ded
Håvard Berland <havb@equinor.com>
2021-01-19 15:17:32 +0100

    Fully reinstate etc.py, but deprecated

--- tests/test_etc.py
+++ tests/test_etc.py
@@ -7,10 +7,12 @@ import pytest
 
 
 def test_deprecated_etc():
-    """The following lines was in documentation up to 1.4.0 inclusive
+    """The following lines was in documentation up to 1.4.0 inclusive.
 
-    The should just not crash, the logging configuration does not have
-    to work."""
+    v1.4.1 and v1.4.2 miss support for fmu.ensemble.etc.Interaction()
+    and then it is reinstated in v1.4.3. To be removed for fmu.ensemble v2.0.0
+    """
+
     with pytest.warns(DeprecationWarning):
         fmux = ensemble.etc.Interaction()
     logger = fmux.basiclogger(__name__, level="WARNING")
